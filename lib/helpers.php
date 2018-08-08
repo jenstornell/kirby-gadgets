920fd55c7e4f8de9a09874bbde302bdc32f6fbfc
Jens Törnell <jens.tornell@gmail.com>
2018-08-08 21:04:26 +0200

    slug

--- lib/helpers.php
+++ lib/helpers.php
@@ -7,4 +7,29 @@ class gadget {
   function isCollection($collection) {
     return(is_a($collection, 'Kirby\Cms\Pages'));
   }
+
+  function slug($string) {
+    return strtolower(
+      trim(
+        preg_replace(
+            '~[^0-9a-z]+~i',
+            '-',
+            html_entity_decode(
+            preg_replace(
+                '~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i',
+                '$1',
+                htmlentities(
+                $string,
+                ENT_QUOTES,
+                'UTF-8'
+                )
+            ),
+            ENT_QUOTES,
+            'UTF-8'
+            )
+        ),
+        '-'
+        )
+    );
+  }
 }
