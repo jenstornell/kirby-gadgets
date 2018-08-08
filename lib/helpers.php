1b814302d9321112d3654b61545cbf75950eaf7b
Jens Törnell <jens.tornell@gmail.com>
2018-08-08 20:55:29 +0200

    Is collection

--- lib/helpers.php
+++ lib/helpers.php
@@ -3,4 +3,8 @@ class gadget {
   function isLocalhost($whitelist = ['127.0.0.1', '::1']) {
     return in_array($_SERVER['REMOTE_ADDR'], $whitelist);
   }
+
+  function isCollection($collection) {
+    return(is_a($collection, 'Kirby\Cms\Pages'));
+  }
 }
